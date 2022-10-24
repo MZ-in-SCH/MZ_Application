b54cfebe64fd5cd3940e44c3a76773ea2e72c6c7
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2022-10-24 16:03:56 +0900

    일단은 사진들 찾은거 뉴스 적당한게 왜이리 없냐

--- App.js
+++ App.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 
-import KeyProvider from './Components/KeyStore'
+import KeyProvider from './Components/KeyStore';
 
-import LoginScreen from './Components/Login/LoginScreen'
-import SignupScreen from './Components/Signup/SignupScreen'
+import LoginScreen from './Components/Login/LoginScreen';
+import SignupScreen from './Components/Signup/SignupScreen';
 import FavoriteSelectScreen from './Components/Signup/FavoriteSelectScreen';
-import Title from './Components/Navigation/TabNavigation'
+import Title from './Components/Navigation/TabNavigation';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const React$Node = () => {
     return (
-        <NavigationContainer>
-          <KeyProvider>
-            <Stack.Navigator initialRouteName="Login">
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Signup" component={SignupScreen} />
-              <Stack.Screen name="Favorite" component={FavoriteSelectScreen} />
-              <Stack.Screen name="Title" options={{ headerShown: false }} component={Title} />
-            </Stack.Navigator>
-          </KeyProvider>
-        </NavigationContainer>
+      <NavigationContainer>
+        <KeyProvider>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+            <Stack.Screen name="Favorite" component={FavoriteSelectScreen} />
+            <Stack.Screen
+              name="Title"
+              options={{headerShown: false}}
+              component={Title}
+            />
+          </Stack.Navigator>
+        </KeyProvider>
+      </NavigationContainer>
     );
   };
   return React$Node();
